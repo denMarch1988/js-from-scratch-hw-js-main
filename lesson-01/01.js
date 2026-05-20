7903630a3efac6268ab99c1c3a170edabe487f61
denMarch1988 <denis.marchienko.1988@gmail.com>
2026-05-20 20:11:13 +0300

    complete task-2

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,11 +2,11 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum = 0
-let number = 1
+let sum = 0;
+let number = 1;
 while (number < 21) {
     sum += number
     number += 1
-}
+};
 
-console.log(sum)
+console.log(sum);
