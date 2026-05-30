c1cc0dcfd8503a9da113efb85aa6a70632c7a20a
denMarch1988 <denis.marchienko.1988@gmail.com>
2026-05-30 14:29:17 +0300

    complete task-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,14 +6,14 @@
 
 const score = 70 // тестовое значение, можно изменять
 let grade
-if (score >= 0 && score < 50) {
+if (score <= 0 && score < 50) {
     console.log("F")
-} else if (score >= 50 && score < 70) {
+} else if (score <= 50 && score < 70) {
     console.log("D")
-} else if (score >= 70 && score < 80) {
+} else if (score <= 70 && score < 80) {
     console.log("C")
-} else if (score >= 80 && score < 90) {
+} else if (score <= 80 && score < 90) {
     console.log("B")
-} else {
+} else if (score <= 90 && score < 100) {
     console.log("A")
 } // your code
