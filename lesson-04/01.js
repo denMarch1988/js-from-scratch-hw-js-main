ea9f03324db7b008d9f8364eabf6e67efdfa82d5
denMarch1988 <denis.marchienko.1988@gmail.com>
2026-07-11 10:13:13 +0300

    10

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,14 +13,3 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement(array, element) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-            return true
-            break
-        }
-    }
-    return false
-}
-
-console.log(includesElement([1, 2, 3], 3));
