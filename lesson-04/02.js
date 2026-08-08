faf2aecd3b3cbb14564b2a58124a949fc0b8ab1c
denMarch1988 <denis.marchienko.1988@gmail.com>
2026-08-08 19:40:59 +0300

    complete task

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -14,16 +14,11 @@
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+
 array = [1, 2, 3, 2, 1, 4];
 
 function findUniqueElements(array) {
-    const newArr = [];
-    for (let i = 0; i < array.length; i++) {
-        if (newArr.indexOf(array[i]) === -1) {
-            newArr.push(array[i])
-        }
-    }
-    return newArr;
+    return [...new Set(array)];
 }
 
 console.log(findUniqueElements(array));
